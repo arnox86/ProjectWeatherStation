ebfa39a43b5bbb41925e512b0bb0d17691f94afb
arnox86 <63281428+arnox86@users.noreply.github.com>
2020-04-08 19:13:26 +0200

    Update dht11.h

--- DHT11 Temp and humidity/dht11.h
+++ DHT11 Temp and humidity/dht11.h
@@ -1,1 +1,28 @@
+/*
+*   
+*/
 
+#ifndef dht11_h
+#define dht11_h
+
+#include "Arduino.h"
+
+class dht11 {
+  
+  public:
+    dht11(int pin);
+    double measure(String out_type);
+  
+  private:
+    int _com_pin;
+    int _data_out[5];
+    int _measure_counter;
+    double _output;
+  
+    byte _measure_run();
+    byte _input_value;
+    int _count;
+  
+};
+
+#endif
