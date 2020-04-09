32392235a33fb0c14923b49fd122a0d08797cc58
arnox86 <63281428+arnox86@users.noreply.github.com>
2020-04-09 21:42:50 +0200

    Update exampleCodeLibrary.ino

--- DHT11 Temp and humidity/exampleCodeLibrary.ino
+++ DHT11 Temp and humidity/exampleCodeLibrary.ino
@@ -1,1 +1,22 @@
 
+#include <dht11.h>
+// #include "dht11.h"   // Use this one if your library is saved in the sketch folder
+
+
+dht11 DHT11 (14);       // Put your communication pin in here; 14 is analog pin 0 at Arduino Uno
+
+void setup () {
+  
+  Serial.begin (9600);  // Put your serial baud rate in here
+  
+}
+
+void loop () {
+  
+  Serial.println (DHT11.temp());
+  Serial.println (DHT11.hum());
+  delay (500);
+  
+  exit (0);
+  
+}
