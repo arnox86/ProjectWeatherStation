2020859c1d7ef68611efb104ff3cedd595112a59
arnox86 <63281428+arnox86@users.noreply.github.com>
2020-04-11 21:00:23 +0200

    Added debug structure

--- SN74HC595N/shiftReg.h
+++ SN74HC595N/shiftReg.h
@@ -6,10 +6,26 @@
 #include "Arduino.h"
 
 
+/* Uncomment for some nice debug helps */
+//#define DEBUG_MESSAGE
+
+#define DEBUG_BEGIN Serial    // Output type of debug messages
+
+#ifndef DEBUG_MESSAGE
+  #define DEBUG_PRINT (...) { DEBUG_BEGIN.print (__VA_ARG__); }
+  #define DEBUG_PRINTLN (...) { DEBUG_BEGIN.println (__VA_ARG__); }
+
+#else
+  #define DEBUG_PRINT (...) {}    // Placeholder if disabled
+  #define DEBUG_PRINTLN (...) {}
+
+#endif
+
+
 class shiftReg {
   
   public:
-    shiftReg (int data_pin, int clock_pin, int oe_pin, int register_size, boolean bit_first);
+    shiftReg (int data_pin, int clock_pin, int oe_pin, int register_size, boolean MSB = true);
     void shift (int shift_data);
   
   private:
