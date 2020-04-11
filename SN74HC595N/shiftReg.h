e088adf758aeb31f7db18e8f5fee40fe47facd21
arnox86 <63281428+arnox86@users.noreply.github.com>
2020-04-11 18:12:04 +0200

    Coded some stuff

--- SN74HC595N/shiftReg.h
+++ SN74HC595N/shiftReg.h
@@ -1,4 +1,20 @@
 
 
 #ifndef shiftReg_h
-#
+#define shiftReg_h
+
+#include "Arduino.h"
+
+
+class shiftReg {
+  
+  public:
+    shiftReg (int data_pin, int clock_pin, int oe_pin, int register_size, boolean bit_first);
+    void shift (int shift_data);
+  
+  private:
+    
+  
+}
+
+#endif
