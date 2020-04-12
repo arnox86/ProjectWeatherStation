c07451b0c453f572f01193e569d099a50bf9d0c2
arnox86 <63281428+arnox86@users.noreply.github.com>
2020-04-12 17:23:03 +0200

    Created code

--- SN74HC595N/exampleCodeLibrary.ino
+++ SN74HC595N/exampleCodeLibrary.ino
@@ -1,1 +1,17 @@
 
+#include <shiftReg.h>
+
+
+shiftReg reg1 (9, 10, 11, 8, 0);   // data pin , clock pin, output enable pin, size of register, MSB (1) or LSB (0)
+
+void setup () {
+}
+
+
+void loop () {
+  
+  reg1.shift (0xFF);    // Set all outputs 1
+  
+  exit (0);
+  
+}
