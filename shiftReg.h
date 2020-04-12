331448d2700f7e066f2835d96a527f607b50f038
arnox86 <63281428+arnox86@users.noreply.github.com>
2020-04-12 15:28:03 +0200

    Update shiftReg.h

--- shiftReg.h
+++ shiftReg.h
@@ -9,11 +9,16 @@
 class shiftReg {
   
   public:
-    shiftReg (int data_pin, int clock_pin, int oe_pin, int register_size, boolean MSB = true);
+    shiftReg (int data_pin, int clock_pin, int oe_pin, int register_size, bool MSB = true);
     void shift (int shift_data);
   
   private:
-    
+    boolean _data_bit[128];
+    int _data_pin;
+    int _clock_pin;
+    int _oe_pin;
+    int _register_size;
+    bool _MSB;
   
 }
 
