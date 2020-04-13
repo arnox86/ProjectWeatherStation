00b8355a99a836b8e9cfc1d592a42c00b02e4e5b
arnox86 <63281428+arnox86@users.noreply.github.com>
2020-04-13 20:40:40 +0200

    Complete new layout

--- shiftReg.h
+++ shiftReg.h
@@ -6,8 +6,12 @@
 #include "Arduino.h"
 
 
-#define OUT_TYPE Serial   // Output type
-#define MAX_SIZE 128    // Maximum shift register size
+#define VAR_SIZE    0x08
+#define OUT_TYPE    Serial   // Output type
+#define MAX_SIZE    0x10    // Maximum register size in bytes
+
+#define ON      0xFF
+#define OFF     0x00
 
 #define SHIFT_REGISTER_INDICATOR    // For usage of 1602A
 
@@ -15,20 +19,22 @@
 class shiftReg {
   
   public:
-    shiftReg (int data_pin, int clock_pin, int oe_pin, int register_size, bool MSB = true);
-    void shift (int shift_data);
+    shiftReg (int data_pin, int clock_pin, int oe_pin, int register_size, bool MSB);
+    void shiftData (byte shift_data[MAX_SIZE]);
+    void updateRegister ();
+    void allOne ();
+    void allZero ();
+  
+    byte readOut (int byte_number);
   
   private:
-    bool _data_bit[MAX_SIZE];   // Storage for binary data
-    bool _data_buffer[MAX_SIZE];   // Buffer if MSB is enabled
-  
-    int _data_pin;    
+    int _data_pin;    // Private variables for pins
     int _clock_pin;
     int _oe_pin;
     int _register_size;
     bool _MSB;
   
-    int _shift_data;
+    byte _shift_data[MAX_SIZE];
   
 };
 
