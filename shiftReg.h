889ed46ebcffaaff669fbfe0f0d7f15754aec102
arnox86 <63281428+arnox86@users.noreply.github.com>
2020-04-14 08:19:31 +0200

    Debugged

--- shiftReg.h
+++ shiftReg.h
@@ -8,7 +8,6 @@
 
 #define VAR_SIZE    0x08
 #define OUT_TYPE    Serial   // Output type
-#define MAX_SIZE    0x10    // Maximum register size in bytes
 
 #define ON      0xFF
 #define OFF     0x00
@@ -20,12 +19,12 @@ class shiftReg {
   
   public:
     shiftReg (int data_pin, int clock_pin, int oe_pin, int register_size, bool MSB);
-    void shiftData (byte shift_data[MAX_SIZE]);
+    void shiftData (byte shift_data);
     void updateRegister ();
     void allOne ();
     void allZero ();
   
-    byte readOut (int byte_number);
+    byte readOut ();    // Reading out shift_data
   
   private:
     int _data_pin;    // Private variables for pins
@@ -34,7 +33,7 @@ class shiftReg {
     int _register_size;
     bool _MSB;
   
-    byte _shift_data[MAX_SIZE];
+    byte _shift_data;
   
 };
 
