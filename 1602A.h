9f635441b88475f134ebc9a87e2ec6f38adfdf8a
Bene <63281428+arnox86@users.noreply.github.com>
2020-04-24 20:41:41 +0200

    Update 1602A.h

--- 1602A.h
+++ 1602A.h
@@ -7,6 +7,7 @@
 #define 1602A_h
 
 #include "Arduino.h"
+#include "shiftReg.h"
 
 
 #ifndef SHIFT_REGISTER_INDICATOR
@@ -18,6 +19,9 @@
   #define SH_REG 1
 
 #endif
+
+
+//#define OUT_TYPE Serial    // Output type
 
 
 class 1602A {
