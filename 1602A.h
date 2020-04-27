5d2c36f4ff60f94e0df6d7663aded017d011e2bb
Bene <63281428+arnox86@users.noreply.github.com>
2020-04-27 14:04:59 +0200

    Updated

--- 1602A.h
+++ 1602A.h
@@ -23,22 +23,41 @@
 
 
 #ifndef OUT_TYPE
-
   #define OUT_TYPE Serial    // Output type
+#endif
 
+
+#ifndef ON
+  #define ON 0xFF
+#endif
+
+#ifndef OFF
+  #define OFF 0b00000000    // To be sure ;)
 #endif
 
 
 class 1602A {
   
   public:
-  1602A (     );
+    uint16_t pin_input[8];
+  
+    1602A (pin_input[0], pin_input[1], pin_input[2], pin_input[3], pin_input[4], pin_input[5], pin_input[6], pin_input[7], 
+           uint16_t enable_pin, uint16_t rs_pin, uint16_t bright_pin, uint16_t brightness, 
+           uint8_t operation_mode);
+  
+    void initShiftRegister (uint16_t data_pin, uint16_t clock_pin, uint16_t latch_pin);
     
   
   private:
+    uint16_t _dataPin[8];
+    uint16_t _pbuffer[10];
     
-    
+    uint8_t _sr_assignment[8];    // Assignment of output and output pins
+    uint8_t _sr_output;   // Output byte for shift register
   
+    bool _sr_init;    // Indicates if shift register is used, 0 by default
+    bool _8bit_mode;
+    
 }
 
 
