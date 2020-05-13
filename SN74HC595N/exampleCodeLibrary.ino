ff3229555d0ad1f33843d6ff911d78fcde027ff6
Bene <63281428+arnox86@users.noreply.github.com>
2020-05-13 14:34:30 +0200

    Update exampleCodeLibrary.ino

--- SN74HC595N/exampleCodeLibrary.ino
+++ SN74HC595N/exampleCodeLibrary.ino
@@ -13,16 +13,16 @@ void setup () {
 
 void loop () {
   
-  sr.shiftData (0b10101010);    // Setting an output
+  sr.shiftData (0b10101010);    // Set an output
   delay (1);
   
-  sr.allOne ();   // Sets all output pins to 1
+  sr.allOne ();   // Set all output pins to 1
   delay (1);
   
-  sr.allZero ();    // Sets all output pins to 0
+  sr.allZero ();    // Set all output pins to 0
   delay (1);
   
-  uint8_t shiftRegister_value = sr.readOut ();    // Gives out data in the shift register
+  uint8_t shiftRegister_value = sr.readOut ();    // Give out data in the shift register
   delay (1);
   
   exit (0);
